abe5c0d72cba7549f07b27178d71beec483e8a61
marcusstampp <marcusstampp@gmail.com>
2024-08-28 17:19:31 -0400

    combining-data-types

---
+++ combining-data-types/index.js
@@ -0,0 +1,25 @@
+console.log("cheese" + 42)
+
+console.log("cheese" + 42 + "4" + 2)
+
+console.log("pie" + Boolean(true))
+
+console.log(8 + Boolean(false))
+
+console.log(8 + Boolean(true))
+
+// True is evlauted to 8 and False reprints the number
+
+
+
+
+console.log(null + 7)
+console.log(undefined + 7)
+
+console.log(null + "cat")
+console.log(undefined + "cat")
+
+
+console.log("Hello" - 2)
+console.log(Boolean(true) - 17)
+console.log(Boolean(false) - 17)
